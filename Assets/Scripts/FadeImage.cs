902ab0d9192de37e229a0daa2b58c517de6e75fc
Robert Heslin <robert.heslin.2@gmail.com>
2022-06-09 10:58:12 -0400

    Folders structured a little more. Splash screen added.

---
+++ Assets/Scripts/FadeImage.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeImage : MonoBehaviour
+{
+
+    [SerializeField] GameObject blackOutSquare;
+    float duration = 0.8f;
+
+    private void Start()
+    {
+        StartCoroutine(FadeBlackOutSquare(false, duration));
+    }
+
+    public IEnumerator FadeBlackOutSquare(bool fadeToBlack, float fadeSpeed)
+    {
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if(fadeToBlack)
+        {
+            while(blackOutSquare.GetComponent<Image>().color.a < 1f)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+            SceneManager.LoadScene((int)SceneHandler.LEVELS.mainMenu);
+        }
+        else
+        {
+            while(blackOutSquare.GetComponent<Image>().color.a > 0f)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+            yield return new WaitForSeconds(3f);
+            StartCoroutine(FadeBlackOutSquare(true, duration));
+            
+        }
+
+    }
+
+}
